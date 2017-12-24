ec9bb9bdb412b3af1d5152036bd32cf6bd0a04a8
Aux <justin.gentry@auxesis.tv>
2017-12-23 21:02:21 -0600

    Add this for some reason

---
+++ src/Extensions/CollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Colliebot
+{
+    public static class CollectionExtensions
+    {
+        public static IEnumerable<T> Paginate<T>(this IEnumerable<T> collection, int offset = Constants.DefaultPageOffset, int limit = Constants.DefaultPageSize)
+            => collection.Skip(offset).Take(limit);
+    }
+}
